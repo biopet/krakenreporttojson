86e7933f98140ec5cb974c68c17e8b43cc24380e
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:34:34 +0200

    Fixed compile issue

--- src/test/scala/nl/biopet/tools/krakenreporttojson/KrakenReportToJsonTest.scala
+++ src/test/scala/nl/biopet/tools/krakenreporttojson/KrakenReportToJsonTest.scala
@@ -7,7 +7,7 @@ object KrakenReportToJsonTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
-      ToolTemplate.main(Array())
+      KrakenReportToJson.main(Array())
     }
   }
 }
