309baa9d21983e40b94c34df29790f1ab7790d2c
pjvan_thof <pjrvanthof@gmail.com>
2017-10-12 15:03:43 +0200

    Add code for KrakenReportToJson

--- src/main/scala/nl/biopet/tools/krakenreporttojson/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/krakenreporttojson/ArgsParser.scala
@@ -5,10 +5,22 @@ import java.io.File
 import nl.biopet.utils.tool.AbstractOptParser
 
 class ArgsParser(cmdName: String) extends AbstractOptParser[Args](cmdName) {
-  opt[File]("inputFile")
-    .abbr("i")
-    .unbounded()
-    .required()
-    .maxOccurs(1)
-    .action((x, c) => c.copy(inputFile = x))
+  head(s"""
+          |$cmdName - Convert Kraken-report (full) output to JSON
+      """.stripMargin)
+
+  opt[File]('i', "krakenreport") required () unbounded () valueName "<krakenreport>" action {
+    (x, c) =>
+      c.copy(krakenreport = x)
+  } validate { x =>
+    if (x.exists) success else failure("Krakenreport not found")
+  } text "Kraken report to generate stats from"
+
+  opt[File]('o', "output") unbounded () valueName "<json>" action { (x, c) =>
+    c.copy(outputJson = Some(x))
+  } text "File to write output to, if not supplied output go to stdout"
+
+  opt[Boolean]('n', "skipnames") unbounded () valueName "<skipnames>" action { (x, c) =>
+    c.copy(skipNames = x)
+  } text "Don't report the scientific name of the taxon."
 }
